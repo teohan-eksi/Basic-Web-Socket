00fedd2a801907e6bd1b9945ec0c8993aee45dc1
teohan-eksi <75734275+teohan-eksi@users.noreply.github.com>
2021-06-01 11:25:08 +0300

    implemented messaging btw client and server applications

--- client.js
+++ client.js
@@ -1,4 +1,11 @@
 
 
 
+console.log("client.js runs");
+
 let ws = new WebSocket("ws://localhost:8080");
+
+ws.onmessage = message => console.log("message: " + message.data);
+
+//sending message to the server
+//ws.send("message from client");
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 
+console.log("index.js runs");
 
 const http = require("http");
 const WebSocketServer = require("websocket").server;
@@ -15,15 +16,21 @@ const webSocket = new WebSocketServer({
 });
 
 webSocket.on("request", request=>{
+  console.log("webSocket on request");
+
   connection = request.accept(null, request.origin);
 
   connection.on("onopen", () => {
     console.log("web socket connection opened.");
   });
 
-  connection.on("onmessage", message => {
-    console.log('message: ${message}');
+  connection.on("message", message => {
+    console.log(message.utf8Data);
   });
+
+  //sending message to the client
+  connection.send("message from server");
+
 });
 
 httpServer.listen(8080, ()=>{
