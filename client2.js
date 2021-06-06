3f16731074bf2fa64073cf9bea7283bb12db1420
teohan-eksi <75734275+teohan-eksi@users.noreply.github.com>
2021-06-06 12:42:58 +0300

    added second client.

--- client2.js
+++ client2.js
@@ -1,14 +1,13 @@
 
 
 
-console.log("client1 runs");
+console.log("client2 runs");
 
 let ws = new WebSocket("ws://localhost:8080");
 
-
 document.getElementById("client-btn").addEventListener("click", ()=>{
   //sending message to the server
-  ws.send("message from client1");
+  ws.send("message from client2");
 });
 
 let messageElem = document.getElementById("get-m");
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 
+
 console.log("index.js runs");
 
 const http = require("http");
